e99e6dc7629f664197de8449b039ca01f36d3bde
Aybars Türel <aybarsturel@192.168.1.109>
2026-06-20 22:27:55 +0300

    Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   eactStrictMode: true,
   swcMinify: true,
+  output: 'export', // statik HTML çıktısı için
+  trailingSlash: true,
   eslint: {
     // Derleme (build) sırasında ESLint hataları/uyarıları olsa bile derlemeyi durdurmaz
     ignoreDuringBuilds: true,
